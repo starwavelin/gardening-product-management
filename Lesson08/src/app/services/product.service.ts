993c1dbe4bbd181c39e64625aa01044f2bf4c55b
“starwavelin” <benjaminxianlin@gmail.com>
2022-04-17 21:49:39 -0400

    chore: Lesson08: rm redundant products var

--- Lesson08/src/app/services/product.service.ts
+++ Lesson08/src/app/services/product.service.ts
@@ -12,39 +12,6 @@ export class ProductService {
 
     constructor(private http: HttpClient) {}
 
-    products: Array<IProduct> = [
-        {
-            "productId": 1,
-            "productName": "Leaf Rake (树叶耙)",
-            "productCode": "GDN-0011",
-            "releaseDate": "March 19, 2021",
-            "description": "Leaf rake with 48-inch wooden handle.",
-            "price": 19.95,
-            "starRating": 3.2,
-            "imageUrl": "assets/images/leaf_rake.png"
-        },
-        {
-            "productId": 2,
-            "productName": "Garden Cart (手推车)",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2021",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "assets/images/garden_cart.png"
-        },
-        {
-            "productId": 5,
-            "productName": "Hammer (锤子)",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2021",
-            "description": "Curved claw steel hammer",
-            "price": 8.93,
-            "starRating": 4.8,
-            "imageUrl": "assets/images/hammer.png"
-        }
-    ];
-
     getProducts(): Observable<IProduct[]> {
         return this.http.get<IProduct[]>(this.productUrl).pipe(
             tap(data => console.log('All: ', JSON.stringify(data))),
