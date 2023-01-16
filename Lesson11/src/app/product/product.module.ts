fa109308b65658ccb16cc37dbc5467e394011ebc
“starwavelin” <benjaminxianlin@gmail.com>
2023-01-16 16:26:09 -0500

    feat: Lesson11 - set up shared module

--- Lesson11/src/app/product/product.module.ts
+++ Lesson11/src/app/product/product.module.ts
@@ -1,27 +1,21 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ProductListComponent } from '../components/product-list/product-list.component';
-import { StarComponent } from '../components/star/star.component';
-import { ConvertToSpacesPipe } from '../pipes/convert-to-spaces.pipe';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
 import { ProductDetailComponent } from '../components/product-detail/product-detail.component';
 import { ProductDetailGuard } from '../guards/product-detail.guard';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
     ProductListComponent,
-    StarComponent,
-    ConvertToSpacesPipe,
-    ProductDetailComponent,
+    ProductDetailComponent
   ],
   imports: [
-    CommonModule,
-    FormsModule,
     RouterModule.forChild([
       { path: 'products', component: ProductListComponent },
       { path: 'products/:id', component: ProductDetailComponent, canActivate: [ProductDetailGuard] }
-    ])
+    ]),
+    SharedModule
   ]
 })
 export class ProductModule { }
